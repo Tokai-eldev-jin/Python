26a7a585f8b17574843235b1d2307e72f37f4c06
Teruyuki JInkawa <97813555+Tokai-eldev-jin@users.noreply.github.com>
2023-04-27 16:57:18 +0900

    Delete hairetu.py

--- hairetu.py
+++
@@ -1,8 +0,0 @@
-selectpopup = list(range(2))
-selectpopup[0]="apple"
-selectpopup[1]="orange"
-
-selectpopup2 = list()
-selectpopup2.append("apple")
-selectpopup2.append("orange")
-selectpopup2.append("peach")
